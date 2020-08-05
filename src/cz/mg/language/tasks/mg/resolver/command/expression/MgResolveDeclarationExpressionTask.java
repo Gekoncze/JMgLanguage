20765a8775261ec9a4cb9ff0b2d803e891adf9dd
Me <gekoncze@seznam.cz>
2020-08-05 17:13:12 +0200

    added resolve declaration task

--- src/cz/mg/language/tasks/mg/resolver/command/expression/MgResolveDeclarationExpressionTask.java
+++ src/cz/mg/language/tasks/mg/resolver/command/expression/MgResolveDeclarationExpressionTask.java
@@ -1,8 +1,13 @@
 package cz.mg.language.tasks.mg.resolver.command.expression;
 
+import cz.mg.collections.text.ReadableText;
 import cz.mg.language.annotations.task.Input;
 import cz.mg.language.annotations.task.Output;
 import cz.mg.language.entities.mg.logical.parts.expressions.MgLogicalDeclarationExpression;
+import cz.mg.language.entities.mg.runtime.components.MgVariable;
+import cz.mg.language.entities.mg.runtime.components.types.MgType;
+import cz.mg.language.entities.mg.runtime.other.MgDatatype;
+import cz.mg.language.tasks.mg.resolver.Filter;
 import cz.mg.language.tasks.mg.resolver.contexts.CommandContext;
 
 
@@ -28,6 +33,20 @@ public class MgResolveDeclarationExpressionTask extends MgResolveExpressionTask 
 
     @Override
     protected void onRun() {
-        //todo;
+        expression = new Expression(context, logicalExpression);
+        expression.getDeclaredVariables().addLast(
+            new MgVariable(
+                logicalExpression.getName(),
+                new MgDatatype(
+                    resolveType(logicalExpression.getDatatype().getName()),
+                    MgDatatype.Storage.valueOf(logicalExpression.getDatatype().getStorage().name()),
+                    MgDatatype.Requirement.valueOf(logicalExpression.getDatatype().getRequirement().name())
+                )
+            )
+        );
+    }
+
+    private MgType resolveType(ReadableText name){
+        return new Filter<>(context, MgType.class, name).find();
     }
 }
