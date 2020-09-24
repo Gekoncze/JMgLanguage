93849d17473690f8b8033521e64c9166c33fb8b0
Me <gekoncze@seznam.cz>
2020-09-24 20:56:55 +0200

    removed collection annotations

--- src/cz/mg/language/entities/mg/runtime/parts/commands/MgCommand.java
+++ src/cz/mg/language/entities/mg/runtime/parts/commands/MgCommand.java
@@ -1,18 +1,15 @@
 package cz.mg.language.entities.mg.runtime.parts.commands;
 
 import cz.mg.collections.list.List;
-import cz.mg.language.annotations.entity.CollectionPart;
-import cz.mg.language.annotations.entity.ItemsPart;
-import cz.mg.language.annotations.requirement.CollectionMandatory;
-import cz.mg.language.annotations.requirement.ItemsMandatory;
+import cz.mg.language.annotations.entity.Part;
+import cz.mg.language.annotations.requirement.Mandatory;
 import cz.mg.language.entities.mg.runtime.MgRunnable;
 import cz.mg.language.entities.mg.runtime.components.MgVariable;
 import cz.mg.language.entities.mg.runtime.parts.MgPart;
 
 
 public abstract class MgCommand extends MgPart implements MgRunnable {
-    @CollectionMandatory @CollectionPart
-    @ItemsMandatory @ItemsPart
+    @Mandatory @Part
     private final List<MgVariable> declaredVariables = new List<>();
 
     public MgCommand() {
