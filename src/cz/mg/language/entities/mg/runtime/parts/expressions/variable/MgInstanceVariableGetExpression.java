f217b36a7fb65e4afe2457ab75677d0c616d9e12
Me <gekoncze@seznam.cz>
2020-10-18 11:22:38 +0200

    fixed instance variable expressions also split into get/set variants

--- src/cz/mg/language/entities/mg/runtime/parts/expressions/variable/MgInstanceVariableGetExpression.java
+++ src/cz/mg/language/entities/mg/runtime/parts/expressions/variable/MgInstanceVariableGetExpression.java
@@ -2,21 +2,47 @@ package cz.mg.language.entities.mg.runtime.parts.expressions.variable;
 
 import cz.mg.collections.array.Array;
 import cz.mg.collections.array.ReadableArray;
-import cz.mg.language.annotations.requirement.Mandatory;
+import cz.mg.language.entities.mg.runtime.MgObject;
+import cz.mg.language.entities.mg.runtime.components.types.MgStructuredType;
+import cz.mg.language.entities.mg.runtime.components.variables.MgFunctionVariable;
 import cz.mg.language.entities.mg.runtime.components.variables.MgInstanceVariable;
-import cz.mg.language.entities.mg.runtime.components.variables.MgVariable;
 import cz.mg.language.entities.mg.runtime.instances.MgFunctionInstance;
+import cz.mg.language.entities.mg.runtime.instances.MgStructuredInstance;
+import cz.mg.language.entities.mg.runtime.parts.MgDatatype;
 import cz.mg.language.entities.mg.runtime.parts.connection.MgInputConnector;
 import cz.mg.language.entities.mg.runtime.parts.connection.MgOutputConnector;
 
 
 public class MgInstanceVariableGetExpression extends MgVariableExpression {
-    public MgInstanceVariableGetExpression(MgInstanceVariable variable) {
-        super(todo);
+    public MgInstanceVariableGetExpression(MgStructuredType structuredType, MgInstanceVariable variable) {
+        super(createInputInterface(structuredType), createOutputInterface(variable), variable);
+    }
+
+    @Override
+    public MgInstanceVariable getVariable() {
+        return (MgInstanceVariable) super.getVariable();
     }
 
     @Override
     public void run(MgFunctionInstance functionInstance) {
-        todo;
+        MgFunctionVariable parentVariable = getInputConnectors().getFirst().getConnection().getConnectionVariable();
+        MgStructuredInstance parent = (MgStructuredInstance) functionInstance.getObjects().get(parentVariable.getOffset());
+        MgObject child = parent.getObjects().get(getVariable().getOffset());
+        MgFunctionVariable outputVariable = getOutputConnectors().getFirst().getConnection().getConnectionVariable();
+        functionInstance.getObjects().set(child, outputVariable.getOffset());
+    }
+
+    public static ReadableArray<MgInputConnector> createInputInterface(MgStructuredType structuredType){
+        return new Array<>(new MgInputConnector(
+            new MgDatatype(
+                structuredType,
+                MgDatatype.Storage.OTHER,
+                MgDatatype.Requirement.OPTIONAL
+            )
+        ));
+    }
+
+    public static ReadableArray<MgOutputConnector> createOutputInterface(MgInstanceVariable variable){
+        return new Array<>(new MgOutputConnector(variable.getDatatype()));
     }
 }
--- src/cz/mg/language/entities/mg/runtime/parts/expressions/variable/MgInstanceVariableSetExpression.java
+++ src/cz/mg/language/entities/mg/runtime/parts/expressions/variable/MgInstanceVariableSetExpression.java
@@ -1,16 +1,49 @@
 package cz.mg.language.entities.mg.runtime.parts.expressions.variable;
 
+import cz.mg.collections.array.Array;
+import cz.mg.collections.array.ReadableArray;
+import cz.mg.language.entities.mg.runtime.components.types.MgStructuredType;
+import cz.mg.language.entities.mg.runtime.components.variables.MgFunctionVariable;
 import cz.mg.language.entities.mg.runtime.components.variables.MgInstanceVariable;
 import cz.mg.language.entities.mg.runtime.instances.MgFunctionInstance;
+import cz.mg.language.entities.mg.runtime.instances.MgStructuredInstance;
+import cz.mg.language.entities.mg.runtime.parts.MgDatatype;
+import cz.mg.language.entities.mg.runtime.parts.connection.MgInputConnector;
+import cz.mg.language.entities.mg.runtime.parts.connection.MgOutputConnector;
 
 
 public class MgInstanceVariableSetExpression extends MgVariableExpression {
-    public MgInstanceVariableSetExpression(MgInstanceVariable variable) {
-        super(todo);
+    public MgInstanceVariableSetExpression(MgStructuredType structuredType, MgInstanceVariable variable) {
+        super(createInputInterface(structuredType, variable), createOutputInterface(), variable);
+    }
+
+    @Override
+    public MgInstanceVariable getVariable() {
+        return (MgInstanceVariable) super.getVariable();
     }
 
     @Override
     public void run(MgFunctionInstance functionInstance) {
-        todo;
+        MgFunctionVariable parentVariable = getInputConnectors().getFirst().getConnection().getConnectionVariable();
+        MgFunctionVariable inputValueVariable = getInputConnectors().getLast().getConnection().getConnectionVariable();
+        MgStructuredInstance parent = (MgStructuredInstance) functionInstance.getObjects().get(parentVariable.getOffset());
+        parent.getObjects().set(functionInstance.getObjects().get(inputValueVariable.getOffset()), getVariable().getOffset());
+    }
+
+    public static ReadableArray<MgInputConnector> createInputInterface(MgStructuredType structuredType, MgInstanceVariable variable){
+        return new Array<>(
+            new MgInputConnector(
+                new MgDatatype(
+                    structuredType,
+                    MgDatatype.Storage.OTHER,
+                    MgDatatype.Requirement.OPTIONAL
+                )
+            ),
+            new MgInputConnector(variable.getDatatype())
+        );
+    }
+
+    public static ReadableArray<MgOutputConnector> createOutputInterface(){
+        return new Array<>();
     }
 }
